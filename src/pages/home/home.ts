388e17a3e582b78dbd215ff353625000af2b5238
Student <student@iMac-B202-00.local>
2018-10-18 14:34:06 +0200

    User input obtained (using id and model property)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  private inputModel:string = 'Test';
+
   constructor(public navCtrl: NavController) {
+   
+  }
+
+  public btnTranslateClicked (userInput:string):void {
+    console.log('btn Clicked: ', this.inputModel);
 
   }
 
