d0a9896db9acc4c954e89322eea2a80719078f88
Student <student@iMac-B202-00.local>
2018-10-18 14:49:00 +0200

    TranslationProvider ADDED (also with HttpClientModule import in app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslationProvider } from '../providers/translation/translation';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TranslationProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslationProvider } from '../../providers/translation/translation';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,16 @@ export class HomePage {
 
   private inputModel:string = 'Test';
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private translationProvider: TranslationProvider
+  ) {
    
   }
 
   public btnTranslateClicked (userInput:string):void {
     console.log('btn Clicked: ', this.inputModel);
-
+    this.translationProvider.http;
   }
 
 }
