2505aa326f2d84859502abbf04cefc3bd845c6b9
Student <student@iMac-B202-00.local>
2018-10-25 14:49:18 +0200

    HistoryProvider - persistant storage using Storage Plugin

--- src/providers/history/history.ts
+++ src/providers/history/history.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HistoryRecord } from '../../models/history-record';
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the HistoryProvider provider.
@@ -12,8 +13,19 @@ export class HistoryProvider {
 
   private historyArray:Array<HistoryRecord> = [];
 
-  constructor() {
+  constructor(
+    private storage: Storage,
+  ) {
     console.log('Hello HistoryProvider Provider');
+
+    this.storage.get('history').then(
+      (value) => {
+        if(value){
+          // something were in storage
+          this.historyArray = JSON.parse(value);
+        }
+      }
+    );
   }
 
   public saveToHistory(from:string, to:string):void{
@@ -21,6 +33,8 @@ export class HistoryProvider {
     let record = new HistoryRecord(from, to);
     // push record to history array
     this.historyArray.push(record);
+    // save to persitant storage
+    this.storage.set('history', JSON.stringify(this.historyArray));
   }
 
   public getHistory():Array<HistoryRecord> {
